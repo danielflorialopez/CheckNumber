25586f1e41e6432ccfb6cf1682f4a62c7082a81e
dani <dani@pc>
2021-03-07 14:10:09 +0100

    Alta inicial de tots els arxius de CheckNumber al control de versions

---
+++ CheckNumber/src/com/alumne/checknumber/CheckNumber.java
@@ -0,0 +1,20 @@
+package com.alumne.checknumber;
+
+public class CheckNumber {
+	
+	private double number;
+	
+	public CheckNumber(double num) {
+		this.number = num;
+	}
+	
+	public double comprovaSigne() {
+		double status;
+		
+		if(this.number < 0) status = -1;
+		else if(this.number == 0) status = 0;
+		else status = 1;
+		
+		return status;
+	}
+} 
---
+++ CheckNumber/src/com/alumne/checknumber/TestCheckNumber.java
@@ -0,0 +1,14 @@
+package com.alumne.checknumber;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestCheckNumber {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
